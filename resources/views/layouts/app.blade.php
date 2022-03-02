3dda6ff9f67a01a4a2cc1c554108fbf2235d84bf
Rafa Rivas <rafa.rivas@s3w.es>
2022-03-02 19:59:28 +0100

    fix double tap on touch devices

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+    <div id="app" class="disable-dbl-tap-zoom">
         <main class="py-1">
             @yield('content')
         </main>
